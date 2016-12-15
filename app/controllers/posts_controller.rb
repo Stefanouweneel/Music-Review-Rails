6bb452564f369305ca7c4088ef2b0fec66065648
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-12-15 12:26:34 +0100

    Setup Posts controller and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+       redirect_to :action => 'index'
+    else
+       @posts = Post.all
+       render :action => 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.new(post_params)
+
+    if @post.update_attributes(post_param)
+       redirect_to :action => 'show', :id => @post
+    else
+       @posts = Post.all
+       render :action => 'edit'
+    end
+  end
+
+  def delete
+    Post.find(params[:id]).destroy
+    redirect_to :action => 'index'
+  end
+
+  def book_params
+   params.require(:posts).permit(:text, :picture)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "posts#index"
+
+  get 'posts/index'
+  get 'posts/new'
+  post 'posts/create'
+  patch 'posts/update'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/delete'
+  get 'posts/update'
 end
